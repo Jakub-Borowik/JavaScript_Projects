691497f4ef881b1736ea4a20fbf310191b693377
Jacob123 <qbastar@gmail.com>
2026-06-16 21:57:38 +0200

    The new begginings

---
+++ FirstSteps/index.js
@@ -0,0 +1,9 @@
+//console.log(`Hello`);
+//console.log(`I like zgery`);
+
+//window.alert(`This is an alert`)
+//window.alert(`I like zgery`);
+// Commenting like in java
+
+document.getElementById("MyH1").textContent  = `Hello`;
+document.getElementById("MyP").textContent = `I like zgery`;
